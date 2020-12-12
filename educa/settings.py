adbec9ea82033c92b44a02bd50296b366f77ac03
Joshaline <joshuachinwendu@gmail.com>
2020-12-12 15:24:22 +0100

    made achange in the settings.py file

--- educa/settings.py
+++ educa/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o&)3h3_24+8)00yqq!@g53yr4zgkzduw8ta9+la9w445_pf80_'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
